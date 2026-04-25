8c5baa24133752c0a54cd460b67c6a73a994b792
Josue2004exe <alexpc778@gmail.com>
2026-04-24 22:01:48 -0500

    Subiendo ejercicio de POO

---
+++ variables 2.0/desempaquetado.py
@@ -0,0 +1,9 @@
+# creando datos en tuplas
+datos = ("Flavio", "Morales", 20000000)
+
+# desempaqutado
+nombre,apellido,seguidores = datos
+
+# resultados
+print("----------")
+print(f'Mi nombre es {nombre}, y mi apellido es {apellido}, y tambie tengo un total de seguidores {seguidores} mil')
