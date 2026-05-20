e3c54ea7a64f0e8ad09022f73fdcc656898a4f55
Josue2004exe <alexpc778@gmail.com>
2026-05-19 19:15:38 -0500

    modificado

--- proyecto_personal/models/docente.py
+++ proyecto_personal/models/docente.py
@@ -39,6 +39,16 @@ class Docente(Colaborador):
         self.horas_clase = horas_clase
         self.modalidad = modalidad
         
+    def mostrar_informacion(self):
+        print("\n--- DATOS DE DOCENTE ---")
+        super().mostrar_informacion()
+        print(f"Facultad: {self.facultad}")
+        print(f"Asignatura: {self.asignatura}")
+        print(f"Nivel académico: {self.nivel_academico}")
+        print(f"Horas de clase: {self.horas_clase}")
+        print(f"Modalidad: {self.modalidad}")
+        
+        
     def convertir_a_texto(self):
         datos_colaborador = super().convertir_a_texto()
         return f"{datos_colaborador}, {self.facultad}, {self.asignatura}, {self.nivel_academico}, {self.horas_clase}, {self.modalidad}"
